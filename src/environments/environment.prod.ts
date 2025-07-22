caea20968c50bd7b322f179d162b60001271a8b9
martinriosc <martin.rios@catena.cl>
2025-07-21 20:26:14 -0400

    push

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://ekokai-api-rt685.ondigitalocean.app',
+  apiUrl: 'https://ekokai-backend-zqwmi.ondigitalocean.app',
   version: '1.0.0'
 };
